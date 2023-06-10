df8dd037c4e9c177eece94ae5a1d9de3ade82353
Shantilal Patliya <shantilalpatliya18@gmail.com>
2023-06-11 00:43:59 +0530

    image api added

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,10 +2,23 @@
 
 import { useState } from "react"
 import SearchManufacturer from "./SearchManufacturer"
+import Image from "next/image"
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+        <Image src="/magnifying-glass.svg" alt="mangifying glass"
+            width={40}
+            height={40}
+            className="object-contain"
+        />
+    </button>
+
+)
 
 const SearchBar = () => {
 
     const [manufacturer, setManufacturer] = useState("")
+    const [model, setModel] = useState("")
 
     const handleSearch = () => {
 
@@ -17,7 +30,25 @@ const SearchBar = () => {
                     manufacturer={manufacturer}
                     setManufacturer={setManufacturer}
                 />
+                <SearchButton otherClasses="sm:hidden" />
             </div>
+
+            <div className="searchbar__item">
+                <Image src="/model-icon.png" width={25}
+                    height={25}
+                    className="absolute w-[20px] h-[20px] ml-4"
+                    alt="car model"
+
+                />
+                <input type="text" name="model"
+                    value={model}
+                    onChange={(e) => setModel(e.target.value)}
+                    placeholder="Tiguan"
+                    className="searchbar__input"
+                />
+                <SearchButton otherClasses="sm:hidden" />
+            </div>
+            <SearchButton otherClasses="max-sm:hidden" />
         </form>
     )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 const axios = require('axios');
 
 console.log()
@@ -6,7 +8,7 @@ export async function fetchCars() {
     const options = {
         method: 'GET',
         url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-        params: { model: 'corolla' },
+        params: { model: 'q3' },
         headers: {
             'X-RapidAPI-Key': process.env.XRAPIDAPIKEY,
             'X-RapidAPI-Host': process.env.XRAPIDHOST
@@ -21,3 +23,32 @@ export async function fetchCars() {
     }
 
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage')
+    const { make, year, model } = car;
+
+    url.searchParams.append("customer", "hrjavascript-mastery")
+    url.searchParams.append("make", make)
+    url.searchParams.append("modelFamily", model.split(" ")[0])
+    url.searchParams.append("zoomType", "fulsscreen")
+    url.searchParams.append("modelYear", `${year}`)
+    url.searchParams.append("angle", `${angle}`)
+    return `${url}`
+}
+
