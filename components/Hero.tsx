8aa0c920c6d8eb662802f06f566a591e73fe1176
Shantilal Patliya <shantilalpatliya18@gmail.com>
2023-06-11 08:27:08 +0530

    lazy loading hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
 
             <div className="hero__image-container">
                 <div className="hero__image">
-                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                    <Image src="/hero.png" alt="hero" fill className="object-contain" loading="lazy" />
                 </div>
                 <div className="hero__image-overlay" />
             </div>
