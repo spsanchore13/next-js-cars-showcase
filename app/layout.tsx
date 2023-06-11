e41d7f450cb3dfd5e5d51d5e6d92e62bb5a6dfbe
Shantilal Patliya <shantilalpatliya18@gmail.com>
2023-06-11 08:22:55 +0530

    favicon icon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+
       <body className="relative">
         <Navbar />
         {children}
